2165642cf9be60f6bdc01f083b9f994f67f223bd
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-08-28 17:33:58 +0200

    Only unzipping sciSQL during install as its configure script requires MySQL configuration informations which are not yet available at install time.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,3 +1,15 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-CONFIGURE_OPTIONS="--prefix=$PREFIX --client-only"
+build() { :; }
+install() 
+{
+    die_if_empty PRODUCT
+    die_if_empty VERSION
+
+    clean_old_install
+    mkdir -p "${PREFIX}"
+    cp -r `ls | egrep -v '^ups'` ${PREFIX}/.
+    install_ups
+}
+
+
